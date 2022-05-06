5bb81627509cdcedcef71c020ce3b19ed7222162
RadoZakutny <43524759+RadoslavZakutny@users.noreply.github.com>
2022-05-06 19:39:35 +0200

    fixed relative import

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
 import nuke
-from . import open_recent
+import open_recent
 
 addToMenu = nuke.menu("Nuke").findItem("File/Open Recent Comp")
 addToMenu.addCommand("Show in GUI mode", "open_recent.show_panel()", "alt+o")
